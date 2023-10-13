8df1dc0ebde0eb2964b7a037eaf3420dc7b7d66f
minkawoo <minkawoo@gmail.com>
2023-10-13 10:06:42 +0900

    구구단 함수 완성

---
+++ index.js
@@ -0,0 +1,8 @@
+// 구구단을 출력하는 함수
+const makeMultiplication = () => {
+  for (let i = 0; i <= 9; i++) {
+    for (let j = 1; j <= 9; i++) {
+      console.log(`${i} * ${j} = ${i * j}`);
+    }
+  }
+};
